a36aacb041fd137f721c4fa6d2e7af503fbb8d68
Rodion <shikvmv@gmail.com>
2025-11-08 17:15:58 +0400

    feat: read config file

---
+++ simple_tracker/simple_tracker/main.py
@@ -0,0 +1,10 @@
+config_path = '/home/john/python_homeworks/python_basics/simple_tracker/config/config.txt'
+
+try:
+    with open(config_path, 'r') as f:
+        file_content = f.read()
+        print('File content:\n',file_content)
+except FileNotFoundError:
+    print(config_path, 'is not found')
+except Exception as e:
+    print('An error occured')
